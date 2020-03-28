3fb7e02d3b3c49d33c1641107ff486ef18dfd5bf
Matthew <xuwanjin1991@Gmail.com>
2020-03-28 10:43:05 +0800

    add Inchoate project, redevelop the Economist
    Signed-off-by: Matthew <xuwanjin1991@Gmail.com>

---
+++ Inchoate/app/src/main/java/com/xuwanjin/inchoate/InchoateActivity.java
@@ -0,0 +1,14 @@
+package com.xuwanjin.inchoate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InchoateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

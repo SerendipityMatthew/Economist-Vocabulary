19a7b8e17c9215c59048032976bc5eda436be5d4
Matthew <xuwanjin1991@Gmail.com>
2020-06-14 22:13:45 +0800

    AudioPlayerFragment in Expanded mode, collapsed it when click bottom navigation item
    Signed-off-by: Matthew <xuwanjin1991@Gmail.com>

--- Inchoate/app/src/main/java/com/xuwanjin/inchoate/InchoateActivity.java
+++ Inchoate/app/src/main/java/com/xuwanjin/inchoate/InchoateActivity.java
@@ -20,6 +20,7 @@ import android.widget.FrameLayout;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
+import com.sothree.slidinguppanel.SlidingUpPanelLayout.PanelState;
 import com.xuwanjin.inchoate.events.SlidingUpControllerEvent;
 import com.xuwanjin.inchoate.timber_style.EconomistPlayerTimberStyle;
 import com.xuwanjin.inchoate.timber_style.IEconomistService;
@@ -44,7 +45,7 @@ public class InchoateActivity extends AppCompatActivity implements
     private boolean isSuccess = false;
     private AudioPlayerFragment mAudioPlayerFragment = new AudioPlayerFragment();
     private FragmentManager mFragmentManager = getSupportFragmentManager();
-
+    private PanelState mPanelState = null;
     ServiceConnection economistServiceConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
@@ -52,10 +53,8 @@ public class InchoateActivity extends AppCompatActivity implements
             setEconomistService(mEconomistService);
             isSuccess = true;
             Log.d(TAG, "onServiceConnected: mEconomistService = " + mEconomistService);
-            if (isAudioPlying()){
-                SlidingUpControllerEvent panelState = new SlidingUpControllerEvent();
-                panelState.panelState = SlidingUpPanelLayout.PanelState.COLLAPSED;
-                EventBus.getDefault().post(panelState);
+            if (isAudioPlying()) {
+                collapsedTheAudioPlayerFragment(PanelState.COLLAPSED);
             }
         }
 
@@ -67,6 +66,7 @@ public class InchoateActivity extends AppCompatActivity implements
             isSuccess = false;
         }
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -82,17 +82,18 @@ public class InchoateActivity extends AppCompatActivity implements
         mSlidingUpPanelLayout.addPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener() {
             @Override
             public void onPanelSlide(View panel, float slideOffset) {
-                if (mAudioPlayerFragment.getAudioPlayingBar() != null){
+                if (mAudioPlayerFragment.getAudioPlayingBar() != null) {
                     mAudioPlayerFragment.getAudioPlayingBar().setVisibility(slideOffset > 0.3 ? View.GONE : View.VISIBLE);
                 }
-                if (slideOffset > 0.3){
+                if (slideOffset > 0.3) {
 
                 }
             }
 
             @Override
-            public void onPanelStateChanged(View panel, SlidingUpPanelLayout.PanelState previousState, SlidingUpPanelLayout.PanelState newState) {
-                if (newState == SlidingUpPanelLayout.PanelState.EXPANDED){
+            public void onPanelStateChanged(View panel, PanelState previousState, PanelState newState) {
+                mPanelState = newState;
+                if (newState == PanelState.EXPANDED) {
                     panel.setFocusableInTouchMode(true);
                     panel.requestFocus();
                     panel.setOnKeyListener(new View.OnKeyListener() {
@@ -100,18 +101,15 @@ public class InchoateActivity extends AppCompatActivity implements
                         public boolean onKey(View v, int keyCode, KeyEvent event) {
                             if (event.getAction() == KeyEvent.ACTION_UP
                                     && keyCode == KeyEvent.KEYCODE_BACK) {
-                                SlidingUpControllerEvent panelState = new SlidingUpControllerEvent();
-                                panelState.panelState = SlidingUpPanelLayout.PanelState.COLLAPSED;
-                                EventBus.getDefault().post(panelState);
+                                collapsedTheAudioPlayerFragment(PanelState.COLLAPSED);
                                 return true;
                             }
                             return false;
                         }
                     });
-                }else {
+                } else {
                     panel.setOnKeyListener(null);
                 }
-
             }
         });
     }
@@ -126,8 +124,9 @@ public class InchoateActivity extends AppCompatActivity implements
     protected void onResume() {
         super.onResume();
     }
-    public boolean isAudioPlying(){
-       return EconomistPlayerTimberStyle.isPlaying();
+
+    public boolean isAudioPlying() {
+        return EconomistPlayerTimberStyle.isPlaying();
     }
 
 
@@ -136,8 +135,7 @@ public class InchoateActivity extends AppCompatActivity implements
         inflateAudioPlaying(event.panelState);
     }
 
-    public void inflateAudioPlaying(SlidingUpPanelLayout.PanelState panelState) {
-        Log.d(TAG, "inflateAudioPlaying: panelState = " + panelState);
+    public void inflateAudioPlaying(PanelState panelState) {
         mFragmentManager
                 .beginTransaction()
                 .replace(R.id.now_playing_control, mAudioPlayerFragment)
@@ -148,6 +146,9 @@ public class InchoateActivity extends AppCompatActivity implements
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (mPanelState == PanelState.EXPANDED) {
+            collapsedTheAudioPlayerFragment(PanelState.COLLAPSED);
+        }
         if (item.getItemId() == R.id.item_today) {
             Utils.navigationController(mController, R.id.navigation_today);
         } else if (item.getItemId() == R.id.item_weekly) {
@@ -161,9 +162,16 @@ public class InchoateActivity extends AppCompatActivity implements
         return true;
     }
 
+    private void collapsedTheAudioPlayerFragment(PanelState panelState) {
+        SlidingUpControllerEvent controllerEvent = new SlidingUpControllerEvent();
+        controllerEvent.panelState = panelState;
+//        EventBus.getDefault().post(panelState);
+        inflateAudioPlaying(panelState);
+    }
+
+
     @Override
     public void isShowBottomNavigation(boolean isShow) {
-        Log.d("Matthew", "isShowBottomNavigation: isShow = " + isShow);
         if (isShow) {
             mBottomNavigationView = findViewById(R.id.bottom_navigation);
             mBottomNavigationView.setVisibility(View.VISIBLE);

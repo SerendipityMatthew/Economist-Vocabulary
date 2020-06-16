c38586d964d234a1c8d1d863a80eb201827230d9
Matthew <xuwanjin1991@Gmail.com>
2020-06-16 08:17:39 +0800

    refactor code
    Signed-off-by: Matthew <xuwanjin1991@Gmail.com>

--- Inchoate/app/src/main/java/com/xuwanjin/inchoate/ui/article/ArticleContentAdapter.java
+++ Inchoate/app/src/main/java/com/xuwanjin/inchoate/ui/article/ArticleContentAdapter.java
@@ -139,6 +139,76 @@ public class ArticleContentAdapter extends RecyclerView.Adapter<ArticleContentAd
     public class ViewHolder extends RecyclerView.ViewHolder {
         public TextView paragraphTextView;
         public Paragraph mParagraph;
+        private ActionMode.Callback mCallback = new ActionMode.Callback() {
+            private Menu mMenu;
+
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                menu.clear();
+                mMenu = menu;
+                MenuInflater inflater = mode.getMenuInflater();
+                if (inflater != null) {
+                    inflater.inflate(R.menu.text_select_menu, menu);
+                }
+                return true;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.text_menu_collect) {
+                    splitAndCollectVocabulary();
+                }
+
+                if (itemId == R.id.text_menu_corral) {
+                }
+                // Finish and close the ActionMode
+                mode.finish();
+                return true;
+            }
+
+
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+                mode.getMenu().close();
+            }
+
+        };
+
+        private void splitAndCollectVocabulary() {
+            int wordStart = 0;
+            int wordEnd = paragraphTextView.getText().length();
+            if (paragraphTextView.isFocused()) {
+                final int selStart = paragraphTextView.getSelectionStart();
+                final int selEnd = paragraphTextView.getSelectionEnd();
+
+                wordStart = Math.max(0, Math.min(selStart, selEnd));
+                wordEnd = Math.max(0, Math.max(selStart, selEnd));
+            }
+            int wordStartTemp = wordStart;
+            int wordEndTemp = wordEnd;
+            // Perform your definition lookup with the selected text
+            final CharSequence selectedText = paragraphTextView.getText().subSequence(wordStart, wordEnd);
+            if (selectedText.toString().contains(" ")) {
+                Snackbar.make(paragraphTextView, "please select a word", Snackbar.LENGTH_SHORT).show();
+            } else {
+                Runnable saveVocabulary = new Runnable() {
+                    @Override
+                    public void run() {
+                        collectTheVocabulary(mParagraph, selectedText.toString(), wordStartTemp, wordEndTemp);
+                    }
+                };
+                Executors.newSingleThreadExecutor().submit(saveVocabulary);
+                Snackbar.make(paragraphTextView, selectedText, Snackbar.LENGTH_SHORT).show();
+                Log.d(TAG, "onActionItemClicked: selectedText = " + selectedText);
+            }
+        }
 
         public ViewHolder(@NonNull final View itemView) {
             super(itemView);
@@ -151,124 +221,7 @@ public class ArticleContentAdapter extends RecyclerView.Adapter<ArticleContentAd
             Typeface typeface = ResourcesCompat.getFont(mContext, R.font.milote_textita);
             paragraphTextView.setFocusableInTouchMode(true);
             paragraphTextView.setTypeface(typeface);
-            paragraphTextView.setCustomSelectionActionModeCallback(new ActionMode.Callback() {
-                private Menu mMenu;
-
-                @Override
-                public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-                    menu.clear();
-                    mMenu = menu;
-                    MenuInflater inflater = mode.getMenuInflater();
-                    if (inflater != null) {
-                        inflater.inflate(R.menu.text_select_menu, menu);
-                    }
-                    return true;
-                }
-
-                @Override
-                public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-                    return false;
-                }
-
-                @Override
-                public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-                    int itemId = item.getItemId();
-
-                    if (itemId == R.id.text_menu_collect) {
-                        int wordStart = 0;
-                        int wordEnd = paragraphTextView.getText().length();
-                        if (paragraphTextView.isFocused()) {
-                            final int selStart = paragraphTextView.getSelectionStart();
-                            final int selEnd = paragraphTextView.getSelectionEnd();
-
-                            wordStart = Math.max(0, Math.min(selStart, selEnd));
-                            wordEnd = Math.max(0, Math.max(selStart, selEnd));
-                        }
-                        int wordStartTemp = wordStart;
-                        int wordEndTemp = wordEnd;
-                        // Perform your definition lookup with the selected text
-                        final CharSequence selectedText = paragraphTextView.getText().subSequence(wordStart, wordEnd);
-                        if (selectedText.toString().contains(" ")) {
-                            Snackbar.make(paragraphTextView, "please select a word", Snackbar.LENGTH_SHORT).show();
-                            return false;
-                        } else {
-                            Runnable saveVocabulary = new Runnable() {
-                                @Override
-                                public void run() {
-
-                                    collectTheVocabulary(mParagraph, selectedText.toString(), wordStartTemp, wordEndTemp);
-                                }
-                            };
-                            Executors.newSingleThreadExecutor().submit(saveVocabulary);
-                            Snackbar.make(paragraphTextView, selectedText, Snackbar.LENGTH_SHORT).show();
-                            Log.d(TAG, "onActionItemClicked: selectedText = " + selectedText);
-                        }
-                    }
-
-                    if (itemId == R.id.text_menu_corral){
-                    }
-                    // Finish and close the ActionMode
-                    mode.finish();
-                    return true;
-                }
-
-                private void collectTheVocabulary(Paragraph paragraph,
-                                                  String vocabularyString, int wordStart, int wordEnd) {
-                    if (paragraph == null
-                            || paragraph.paragraph.length() == 0
-                            || "null".equalsIgnoreCase(vocabularyString)) {
-                        return;
-                    }
-                    InchoateDBHelper dbHelper = InchoateDBHelper.getInstance(mContext);
-                    Vocabulary vocabulary = new Vocabulary();
-                    String paragraphText = paragraph.paragraph.toString();
-                    vocabulary.belongedParagraph = paragraphText;
-                    String belongedSentence = getSentence(paragraphText, vocabularyString, wordStart, wordEnd);
-                    vocabulary.belongedSentence = belongedSentence;
-                    vocabulary.belongedArticleTitle = paragraph.articleName;
-                    vocabulary.belongedIssueDate = paragraph.issueDate;
-                    vocabulary.vocabularyContent = vocabularyString;
-                    vocabulary.belongedSectionName = mArticle.section;
-                    vocabulary.belongedArticleUrl = mArticle.articleUrl;
-                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");// HH:mm:ss
-                    //获取当前时间
-                    Date date = new Date(System.currentTimeMillis());
-                    String dateString = simpleDateFormat.format(date);
-                    vocabulary.collectedDate = dateString.substring(0, 11);
-                    vocabulary.collectedTime = dateString.substring(11, dateString.length() - 1);
-
-                    Log.d(TAG, "collectTheVocabulary: vocabularyString = " + vocabularyString);
-                    dbHelper.insertVocabulary(vocabulary);
-                }
-
-                @Override
-                public void onDestroyActionMode(ActionMode mode) {
-                    mode.getMenu().close();
-                }
-
-                public String getSentence(String text, String vocabulary, int start, int end) {
-                    Locale locale = Locale.US;
-                    BreakIterator breakIterator = BreakIterator.getSentenceInstance(locale);
-                    String belongedSentence = "";
-                    breakIterator.setText(text);
-                    int boundaryIndex = breakIterator.first();
-                    int lastBoundaryIndex = boundaryIndex;
-                    while (boundaryIndex != BreakIterator.DONE) {
-                        if (boundaryIndex > 0) {
-                            lastBoundaryIndex = boundaryIndex;
-                        }
-                        boundaryIndex = breakIterator.next();
-                        if (boundaryIndex < 0) {
-                            break;
-                        }
-                        if (lastBoundaryIndex < start && boundaryIndex > start) {
-                            belongedSentence = text.substring(lastBoundaryIndex, boundaryIndex).replace("\n", "").trim();
-                        }
-                    }
-
-                    return belongedSentence;
-                }
-            });
+            paragraphTextView.setCustomSelectionActionModeCallback(mCallback);
         }
 
         public void setCurrentParagraph(Paragraph paragraph) {
@@ -277,15 +230,9 @@ public class ArticleContentAdapter extends RecyclerView.Adapter<ArticleContentAd
 
         private ClickableSpan getClickableSpan(final String word) {
             ClickableSpan clickableSpan = new ClickableSpan() {
-                final String mWord;
-
-                {
-                    mWord = word;
-                }
-
+                final String mWord = word;
                 @Override
                 public void onClick(View widget) {
-                    Log.d("tapped on:", mWord);
                     Toast.makeText(widget.getContext(), mWord, Toast.LENGTH_SHORT)
                             .show();
                 }
@@ -295,6 +242,57 @@ public class ArticleContentAdapter extends RecyclerView.Adapter<ArticleContentAd
                 }
             };
             return clickableSpan;
+        }
+
+        private void collectTheVocabulary(
+                Paragraph paragraph, String vocabularyString, int wordStart, int wordEnd) {
+            if (paragraph == null
+                    || paragraph.paragraph.length() == 0
+                    || "null".equalsIgnoreCase(vocabularyString)) {
+                return;
+            }
+            InchoateDBHelper dbHelper = InchoateDBHelper.getInstance(mContext);
+            Vocabulary vocabulary = new Vocabulary();
+            String paragraphText = paragraph.paragraph.toString();
+            vocabulary.belongedParagraph = paragraphText;
+            String belongedSentence = getSentence(paragraphText, vocabularyString, wordStart, wordEnd);
+            vocabulary.belongedSentence = belongedSentence;
+            vocabulary.belongedArticleTitle = paragraph.articleName;
+            vocabulary.belongedIssueDate = paragraph.issueDate;
+            vocabulary.vocabularyContent = vocabularyString;
+            vocabulary.belongedSectionName = mArticle.section;
+            vocabulary.belongedArticleUrl = mArticle.articleUrl;
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");// HH:mm:ss
+            //获取当前时间
+            Date date = new Date(System.currentTimeMillis());
+            String dateString = simpleDateFormat.format(date);
+            vocabulary.collectedDate = dateString.substring(0, 11);
+            vocabulary.collectedTime = dateString.substring(11, dateString.length() - 1);
+
+            Log.d(TAG, "collectTheVocabulary: vocabularyString = " + vocabularyString);
+            dbHelper.insertVocabulary(vocabulary);
+        }
+
+        public String getSentence(String text, String vocabulary, int start, int end) {
+            Locale locale = Locale.US;
+            BreakIterator breakIterator = BreakIterator.getSentenceInstance(locale);
+            String belongedSentence = "";
+            breakIterator.setText(text);
+            int boundaryIndex = breakIterator.first();
+            int lastBoundaryIndex = boundaryIndex;
+            while (boundaryIndex != BreakIterator.DONE) {
+                if (boundaryIndex > 0) {
+                    lastBoundaryIndex = boundaryIndex;
+                }
+                boundaryIndex = breakIterator.next();
+                if (boundaryIndex < 0) {
+                    break;
+                }
+                if (lastBoundaryIndex < start && boundaryIndex > start) {
+                    belongedSentence = text.substring(lastBoundaryIndex, boundaryIndex).replace("\n", "").trim();
+                }
+            }
+            return belongedSentence;
         }
     }
 }

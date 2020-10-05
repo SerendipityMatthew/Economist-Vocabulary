c921cf184504b52d8b79fe2e7ea5699e97e941c0
Matthew <xuwanjin1991@Gmail.com>
2020-10-05 17:40:06 +0800

    itemheader show problem, scroll to top, transparent itemheader
    Signed-off-by: Matthew <xuwanjin1991@Gmail.com>

--- Inchoate/app/src/main/java/com/xuwanjin/inchoate/ui/weekly/WeeklyItemDecoration.java
+++ Inchoate/app/src/main/java/com/xuwanjin/inchoate/ui/weekly/WeeklyItemDecoration.java
@@ -117,16 +117,9 @@ public class WeeklyItemDecoration extends BaseItemDecoration<WeeklyAdapter> {
 
         String groupName = mAdapter.getGroupName(position);
         int y = mItemHeaderHeight / 2 + mTextRect.height() / 2;
-        if (isHeader) {
-            int bottom = Math.min(mItemHeaderHeight, view.getBottom());
-            canvas.drawRect(0, view.getTop() - mItemHeaderHeight, parent.getWidth(), bottom, mItemHeaderPaint);
-            canvas.drawText(groupName, 50,
-                    y - (mItemHeaderHeight - bottom), mTextPaint);
-        } else {
-            // 如果把下面的注释掉, 会出现即使下一个分类小组没有滑动到顶部, 顶部的 stick header 会消失
-            canvas.drawRect(0, 0, parent.getWidth(), mItemHeaderHeight, mItemHeaderPaint);
-            canvas.drawText(groupName, 50, y, mTextPaint);
-        }
+        canvas.drawRect(0, 0, parent.getWidth(), mItemHeaderHeight, mItemHeaderPaint);
+        canvas.drawText(groupName, 50, y, mTextPaint);
+
     }
 
 }
